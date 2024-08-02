e6d02a317882b6f80983a99c5384d6bfcf285531
marceloreggiani <mreggiani@gmail.com>
2024-08-02 10:41:11 -0300

    acerto da rota de cursos

--- curso-back/routes/cursos.js
+++ curso-back/routes/cursos.js
@@ -1,13 +1,14 @@
 import { Curso } from "../models/curso.js";
+import { Professor } from "../models/professor.js";
 import { Router } from "express";
 
 export const  cursosRouter = Router();
 
 cursosRouter.post("/cursos", async (req, res) => {
-    const { nome, turno, dataInicio } = req.body
+    const { nome, turno, dataInicio, professor } = req.body
 
     try {
-        await Curso.create({ nome, turno, dataInicio });
+        await Curso.create({ nome, turno, dataInicio, professor }, { include: [ Professor ] });
         res.json({message: "curso Criado com Sucesso"});
     } catch (err) {
         res.status(500).json({ message: "Ocorreu um erro ao adicionar curso." });
@@ -15,12 +16,13 @@ cursosRouter.post("/cursos", async (req, res) => {
 });
 
 cursosRouter.put("/cursos/:id", async (req, res) => {
-    const { nome, turno, dataInicio } = req.body;
+    const { nome, turno, dataInicio, professor } = req.body;
 
     try {
         const curso = await Curso.findByPk(req.params.id);
         if(curso) {
             await curso.update({ nome, turno, dataInicio });
+            await Professor.update(professor, {where: { cursoId: idCursos } });
             res.json({ message: "curso atualizado com SUCESSO"})
         } else {
             res.status(404).json({ message: "curso não encontrado" });
